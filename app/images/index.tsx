446261f7ea713d64b97eb4c25a357e48c9e58018
Mohamoud <mohamoud43@gmail.com>
2023-11-13 13:34:31 +0000

    first commit - hero

---
+++ app/images/index.tsx
@@ -0,0 +1,15 @@
+import hero from "../images/hero.png";
+import logo from "../images/zapiapp-wht.png";
+import lp1 from "../images/LP1.svg";
+import lp2 from "../images/LP2.png";
+import lp3 from "../images/LP3.png";
+
+const img = {
+  hero: hero,
+  logo: logo,
+  lp1: lp1,
+  lp2: lp2,
+  lp3: lp3,
+};
+
+export default img;
---
+++ app/types/calendly.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  const Calendly: any;
+}
+
+export {};
